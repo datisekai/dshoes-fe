05e60c5d8f0d2d1dfc8ea8158acf414372c556c2
datisekai <84700751+datisekai@users.noreply.github.com>
2022-05-04 16:09:20 +0700

    change server

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export const base_api =
-  "https://dshoes-server-production-053d.up.railway.app/api";
+  "https://dshoes-server-production-cbe2.up.railway.app/api";
 export const base_auth = `${base_api}/auth`;
 export const base_products = `${base_api}/products`;
 export const base_comments = `${base_api}/comments`;
