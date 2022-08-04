1d379e17e0953e5b56abf499b0ec159544637b8f
datisekai <84700751+datisekai@users.noreply.github.com>
2022-08-04 14:28:33 +0700

    add

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const base_api = "https://dshoes-server-production-cbe2.up.railway.app/api";
+export const base_api = "http://localhost:5098/api";
 export const base_auth = `${base_api}/auth`;
 export const base_products = `${base_api}/products`;
 export const base_comments = `${base_api}/comments`;
